dc6f8d86b2a215a4d065d17e8030fe0c00ff3cae
haisul <ex2252@gmail.com>
2023-11-17 13:13:50 +0800

    2023/11/17

--- src/AutoConnect.cpp
+++ src/AutoConnect.cpp
@@ -4,18 +4,24 @@ AutoConnect *AutoConnect::instance = nullptr;
 AutoConnect::AutoConnect(WiFiClass &WiFi) : pWiFi(WiFi) {
     instance = this;
     pWiFi.onEvent(_wifiEvent);
-    StaticJsonDocument<96> j_wifi = getWifiAuthen();
+}
+
+void AutoConnect::begin() {
+
+    StaticJsonDocument<96> j_wifi = _getWifiAuthen();
     if (!j_wifi.isNull()) {
         String _ssid = j_wifi["SSID"].as<String>();
         String _pass = j_wifi["PASSWORD"].as<String>();
         pWiFi.begin(_ssid.c_str(), _pass.c_str());
+        pWiFi.mode(WIFI_STA);
     } else {
-        pWiFi.beginSmartConfig();
-        Serial.println("beginSmartConfig");
+        if (pWiFi.getMode() == WIFI_STA) {
+            _beginServer();
+        }
     }
 }
 
-StaticJsonDocument<96> AutoConnect::getWifiAuthen() {
+StaticJsonDocument<96> AutoConnect::_getWifiAuthen() {
     if (!initLittleFS()) {
         Serial.println("load wifi data Failed!");
         return StaticJsonDocument<96>();
@@ -59,12 +65,143 @@ void AutoConnect::_wifiEvent(WiFiEvent_t event) {
     Serial.printf("[WiFi-event] event: %d\n", event);
     switch (event) {
     case SYSTEM_EVENT_STA_GOT_IP:
-        Serial.print("\nWifi is connected!\n");
         Serial.println(instance->pWiFi.SSID());
         Serial.println(instance->pWiFi.psk());
         Serial.println(instance->pWiFi.localIP());
         Serial.println(instance->pWiFi.RSSI());
         instance->saveWifiAuthen(instance->pWiFi.SSID().c_str(), instance->pWiFi.psk().c_str());
+        if (instance->wifiConnectedCallback) {
+            instance->wifiConnectedCallback();
+        }
         break;
+    }
+}
+
+void AutoConnect::setWifiConnectedCallback(void (*callback)(void)) {
+    wifiConnectedCallback = callback;
+}
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+void AutoConnect::_beginServer() {
+    Serial.println("beginServer");
+    AutoConnect *params = this;
+    xTaskCreatePinnedToCore(taskFunctionStart, "serverLoop", 8192, (void *)params, 1, &taskHandle, 1);
+}
+
+void AutoConnect::taskFunctionStart(void *pvParam) {
+    AutoConnect *instance = static_cast<AutoConnect *>(pvParam);
+    instance->serverLoop();
+    vTaskDelete(NULL);
+}
+
+void AutoConnect::serverLoop() {
+    unsigned long previousMillis = 0;
+    const long interval = 10000;
+
+    server.begin(80);
+    Serial.print("AP IP address: ");
+    Serial.println(WiFi.softAPIP());
+
+    while (1) {
+        WiFiClient client = server.available();
+        if (client) {
+            while (client.connected()) {
+                if (client.available()) {
+                    String data = client.readStringUntil('\n');
+                    Serial.println("Received data: " + data);
+                    if (data == "\r") {
+                        Serial.println("Reading body:");
+                        String body = "";
+                        while (client.available()) {
+                            char c = client.read();
+                            Serial.print(c);
+                            body += c;
+                        }
+
+                        bool result = _getWifiAuthenFromServer(body);
+                        Serial.println("Client disconnected");
+                        if (result) {
+                            client.println("HTTP/1.1 200 OK");
+                            client.println("Connection: close");
+                            client.println("Content-Type: text/plain");
+                            client.println();
+                            client.println("Received body content: true" + body);
+                            client.stop();
+                            pWiFi.mode(WIFI_STA);
+                            return;
+                        } else {
+                            client.println("HTTP/1.1 200 OK");
+                            client.println("Connection: close");
+                            client.println("Content-Type: text/plain");
+                            client.println();
+                            client.println("Received body content: false" + body);
+                            client.stop();
+                        }
+                    }
+                }
+            }
+        }
+
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= interval) {
+            int numStations = WiFi.softAPgetStationNum();
+            if (numStations > 0) {
+                Serial.print("AP connected. Number of connected stations: ");
+                Serial.println(numStations);
+            }
+            previousMillis = currentMillis;
+        }
+    }
+
+    Serial.println("\nServer Stop");
+}
+
+bool AutoConnect::_getWifiAuthenFromServer(String info) {
+    StaticJsonDocument<96> j_wifi;
+    DeserializationError error = deserializeJson(j_wifi, info);
+    if (error) {
+        Serial.printf("deserialize wifi data Failed: %s\n", error.c_str());
+        return false;
+    }
+
+    String ssid = j_wifi["message"]["SSID"];
+    String password = j_wifi["message"]["PASSWORD"];
+
+    Serial.printf("\nSSID: %s  PASSWORD: %s\n", ssid, password);
+    Serial.println("WIFI data load complete\n");
+
+    bool result = _tryConnect(ssid, password);
+    if (result) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool AutoConnect::_tryConnect(String ssid, String password) {
+    unsigned long previousMillis = 0;
+    const long interval = 500;
+    int connectCount = 0;
+    pWiFi.begin(ssid.c_str(), password.c_str());
+    Serial.print("WiFi Connecting...");
+
+    while (1) {
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= interval) {
+            connectCount++;
+            Serial.print(".");
+            if (pWiFi.status() == WL_CONNECTED) {
+                Serial.println("\nWiFi Connected!");
+                return true;
+            } else if (connectCount > 40) {
+                Serial.println("\nWiFi Connected Faild!");
+                return false;
+            }
+            previousMillis = currentMillis;
+        }
     }
 }
--- src/AutoConnect.h
+++ src/AutoConnect.h
@@ -1,22 +1,42 @@
-#ifndef __WIFICONTROL_H_
-#define __WIFICONTROL_H_
+#pragma once
+
+#ifndef __AUTOCONNECT_H_
+#define __AUTOCONNECT_H_
 #include "ArduinoJson.h"
 #include "LITTLEFSFUN/littlefsfun.h"
-// #include <ArduinoJson.h>
+
 #include <WiFi.h>
 
 class AutoConnect {
 private:
     WiFiClass &pWiFi;
+    WiFiServer server;
 
     static AutoConnect *instance;
     static void _wifiEvent(WiFiEvent_t);
 
+    StaticJsonDocument<96> _getWifiAuthen();
+
+    void (*wifiConnectedCallback)(void);
+
+    ///////////////////////////////////////////////////
+
+    void _beginServer();
+
+    TaskHandle_t taskHandle;
+    static void taskFunctionStart(void *pvParam);
+    void serverLoop();
+
+    bool _getWifiAuthenFromServer(String);
+    bool _tryConnect(String, String);
+
 public:
     AutoConnect(WiFiClass &);
-    StaticJsonDocument<96> getWifiAuthen();
+
+    void begin();
     bool saveWifiAuthen(String, String);
     bool removeWifiAuthen();
+    void setWifiConnectedCallback(void (*callback)(void));
 };
 extern AutoConnect *instance;
 
